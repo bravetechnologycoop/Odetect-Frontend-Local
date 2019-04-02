ac341c9bb57115450e66a99503f3709c2406c993
Sajan Rajdev <25463788+sajanrajdev@users.noreply.github.com>
2019-04-02 16:55:19 -0700

    Integrated Motion and Door sensor realtime socket connection and table display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,6 +21,18 @@ export class HomeComponent implements OnInit {
   Mov_f: string = null
   Mov_s: string = null
 
+  Published_at_m: string = null
+  DeviceID_m: string = null
+  LocationID_m: string = null
+  DeviceType_m: string = null
+  Signal_m: string = null
+
+  Published_at_d: string = null
+  DeviceID_d: string = null
+  LocationID_d: string = null
+  DeviceType_d: string = null
+  Signal_d: string = null
+
 
 
   constructor(/*private data: DataService*/) { }
@@ -43,6 +55,28 @@ export class HomeComponent implements OnInit {
       this.Distance = data.data.distance
       this.Mov_f = data.data.mov_f
       this.Mov_s = data.data.mov_s
+    });
+
+    socket.on('motionstatedata', (datam) => {
+      console.log(datam)
+      this.Published_at_m = datam.data.published_at;
+      this.LocationID_m = datam.data.locationid
+      this.DeviceID_m = datam.data.deviceid
+      this.DeviceType_m = datam.data.devicetype
+      this.Signal_m = datam.data.signal
+    });
+
+    socket.on('doorstatedata', (datad) => {
+      console.log(datad)
+      this.Published_at_d = datad.data.published_at;
+      this.LocationID_d = datad.data.locationid
+      this.DeviceID_d = datad.data.deviceid
+      this.DeviceType_d = datad.data.devicetype
+      this.Signal_d = datad.data.signal
+    });
+
+    socket.on('sessionstatedata', (data) => {
+      console.log(data)
     });
   } 
   
